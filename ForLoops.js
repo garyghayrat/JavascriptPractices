46523d9027ae2603c430c4080958f7d0e46dd9c1
GaryGHayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2021-08-11 14:08:07 -0400

    added a few more files

--- ForLoops.js
+++ ForLoops.js
@@ -1,1 +1,14 @@
-//For Loops
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+    ourArray.push(i);
+}
+
+var myArray = [];
+
+
+for (var i = 1; i < 9; i++) {
+    myArray.push(i);
+}
+
+console.log(myArray);
